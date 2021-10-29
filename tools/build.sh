ce0fe9a40c95c0ab27d83e590236d023482cb67c
gennesseaux <jocelyn@gennesseaux.fr>
2021-10-29 16:30:21 +0200

    build release version

--- tools/build.sh
+++ tools/build.sh
@@ -5,7 +5,7 @@ cd ..
 
 # Build the test project
 cmake -S "test" -B "build/test"
-cmake --build "build/test"
+cmake --build "build/test" --config Release
 
 # run the application
 ./build/test/test_app
